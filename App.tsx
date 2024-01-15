7bbca4ea3ba09a2db40be310300f92d8eeb88053
diego64 <diegoferreira1964@gmail.com>
2024-01-15 17:19:23 -0300

    Configuracao da tag email no APP

--- App.tsx
+++ App.tsx
@@ -8,12 +8,18 @@ import { Routes } from './src/routes';
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
+
 import { CartContextProvider } from './src/contexts/CartContext';
 
 OneSignal.setAppId('683858bf-2059-4381-8ee9-45fb04cc43fd')
 
+OneSignal.setEmail('diegoferreira1964@gmail.com');
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('diegoferreira1964@gmail.com');
 
   return (
     <NativeBaseProvider theme={THEME}>
