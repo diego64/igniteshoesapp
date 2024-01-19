c42e7271bbad8381b5d44b2e19c29640a8103755
diego64 <diegoferreira1964@gmail.com>
2024-01-19 17:26:25 -0300

    Modificacao dos parametros de envio de push notification

--- App.tsx
+++ App.tsx
@@ -1,25 +1,46 @@
-import { StatusBar } from 'react-native';
-import OneSignal from 'react-native-onesignal';
+import { useEffect, useState } from 'react';
+import { StatusBar, Platform } from 'react-native';
+import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
+import { Notification } from './src/components/Notification';
 import { Loading } from './src/components/Loading';
-
-import { tagUserEmailCreate } from './src/notifications/notificationsTags';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+/*
+const oneSignalAppId = Platform.OS === 'ios' ? '2b542c58-e2e9-48f1-8bff-004caefe40c4' : 'dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc';
+OneSignal.setAppId(oneSignalAppId);
+*/
 
 OneSignal.setAppId('683858bf-2059-4381-8ee9-45fb04cc43fd')
 
 OneSignal.setEmail('diegoferreira1964@gmail.com');
 
+OneSignal.promptForPushNotificationsWithUserResponse()
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [notification, setNotification] =  useState<OSNotification>();
 
-  tagUserEmailCreate('diegoferreira1964@gmail.com');
+  tagUserInfoCreate();
+
+  useEffect(() => {
+    const unsubscribe = OneSignal
+    .setNotificationWillShowInForegroundHandler((notificationRecivedEvent: NotificationReceivedEvent) => {
+      const response = notificationRecivedEvent.getNotification();
+
+      setNotification(response)
+    })
+
+    return () => unsubscribe;
+
+  },[])
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -31,6 +52,15 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
+      {
+        notification?.title && 
+        <Notification 
+          title={notification.title} 
+          onClose={() => setNotification(undefined)} 
+        />
+      }
+      
     </NativeBaseProvider>
   );
 }
--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -1,5 +1,8 @@
 import OneSignal from 'react-native-onesignal';
 
-export function tagUserEmailCreate(email: string) {
-  OneSignal.sendTag('user_email', email);
+export function tagUserInfoCreate() {
+  OneSignal.sendTags({
+    'user_name': 'Diego',
+    'user_email': 'diegoferreira1964@gmail.com'
+  });
 }
